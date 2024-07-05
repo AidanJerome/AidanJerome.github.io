1b7ef63fd63d1c6141724247dac6095e89431dc8
Aidan <aidanjeromandal@gmail.com>
2024-07-05 20:41:08 +0800

    initial commit

---
+++ assets/scripts/index.js
@@ -0,0 +1,9 @@
+const div = document.querySelector("div")
+const spanHi = document.querySelector("#hi")
+const spanBye = document.querySelector("#bye")
+
+spanHi.classList.add("new-class")
+spanHi.classList.remove("hi1")
+
+spanHi.style.color = "red"
+spanHi.style.backgroundColor ="red"
